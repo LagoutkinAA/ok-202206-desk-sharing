ca4415b70b16d1fdcd73d417c5a8b45c494c6cfc
Lagutkin A.A <aleksey.lagutkin@open.ru>
2022-09-17 11:09:40 +0300

    create biz and validation modules

--- desk-sharing-biz/build.gradle.kts
+++ desk-sharing-biz/build.gradle.kts
@@ -9,7 +9,12 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 
+    implementation(project(":desk-sharing-common"))
+    implementation(project(":desk-sharing-stub"))
+    implementation(project(":desk-sharing-lib-cor"))
+
     testImplementation(kotlin("test-junit"))
     testImplementation(kotlin("test-common"))
     testImplementation(kotlin("test-annotations-common"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
 }
